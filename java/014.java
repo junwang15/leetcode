80bbba87ffbab593e896170da39bcdb1e8bb479e
Jun Wang <junwang15@users.noreply.github.com>
2015-09-13 00:07:03 -0400

    Update 014.java

--- java/014.java
+++ java/014.java
@@ -7,9 +7,7 @@ public class Solution {
     	for(int idx = 0; idx < strs[0].length(); idx++) {
     		c = strs[0].charAt(idx);
     		for(int j = 1; j < strs.length; j++) {
-    			if(idx >= strs[j].length())	
-    				return prefix.toString();
-    			if(strs[j].charAt(idx) != strs[j-1].charAt(idx))
+    			if(idx >= strs[j].length() || strs[j].charAt(idx) != c)	
     				return prefix.toString();
     		}
     		prefix.append(c);
