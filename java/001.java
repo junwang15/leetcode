b0435a5ef3928c67bb7ec25caefbd39f441f3093
Jun Wang <junwang15@users.noreply.github.com>
2015-09-13 22:12:34 -0400

    Update 001.java
    Add another faster solution

--- java/001.java
+++ java/001.java
@@ -12,3 +12,18 @@ public class Solution {
         return null;       
     }
 }
+
+// solution 2
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for(int i = 0; i < nums.length; i++) {
+			if(map.containsKey(target - nums[i])) 
+				return new int[] {map.get(target - nums[i]), i + 1};
+			else
+				map.put(nums[i], i + 1);
+		}
+		return null;    
+    }
+}
