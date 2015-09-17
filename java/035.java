0d13c6761286a134db721a29413c333254851a94
Jun Wang <junwang15@users.noreply.github.com>
2015-09-16 21:35:11 -0400

    Update 035.java

--- java/035.java
+++ java/035.java
@@ -13,3 +13,20 @@ public class Solution {
         return left;
     }
 }
+
+// solution 2 (faster)
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums.length < 1 || target < nums[0]) return 0;
+        if(target > nums[nums.length - 1])  return nums.length;
+        
+        int left = 0, right = nums.length, mid = 0;
+        while(left <= right) {
+            mid = left + (right - left) / 2;
+            if      (nums[mid] < target)    left = mid + 1;
+            else if (nums[mid] > target)    right = mid - 1;
+            else    return mid;
+        }
+        return target > nums[mid] ? mid + 1: mid;
+    }
+}
