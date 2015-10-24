e51c5ed35831816ea96fd533fb7a48dc0a885ee5
Jun Wang <junwang15@users.noreply.github.com>
2015-10-24 16:52:12 -0400

    Update 005.java

--- java/005.java
+++ java/005.java
@@ -42,7 +42,7 @@ public class Solution {
     	for(int j = i; j < len; j++) {
     		dp[i][j] = s.charAt(i) == s.charAt(j) && (j-i<3 || dp[i+1][j-1]);
         	if(dp[i][j] && ((j-i+1)>longest.length()))
-            		longest = s.substring(i, j+1);
+            longest = s.substring(i, j+1);
     	}
     }
     return longest;
