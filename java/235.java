717fc39809877c75e4c031e012785e5a32280c5d
junwang15 <jun.wangbreath@gmail.com>
2015-12-26 21:33:49 -0500

    Added 234 lowest common ancestor

---
+++ java/235.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class LowestCommonAncestor {
+	// recursive solution
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if(root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }else {
+            return root;
+        }
+    }
+	
+	// iterative solution
+	public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+		while(true) {
+			if(root.val > p.val && root.val > q.val) {
+	            root = root.left;
+	        }else if(root.val < p.val && root.val < q.val) {
+	            root = root.right;	
+	        }else {
+	            return root;
+	        }
+		}
+    }
+}
