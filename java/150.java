76749b315255f711bb87380df1139aff9b5dff22
junwang15 <jun.wangbreath@gmail.com>
2016-01-04 20:40:06 -0500

    Modified 150

--- java/150.java
+++ java/150.java
@@ -14,5 +14,27 @@ public class Solution {
                 stk.push(Integer.valueOf(a[i]));
         }
         return stk.pop();
+    }   
+
+	public int evalRPN2(String[] tokens) {
+		Stack<String> stk = new Stack<String>();
+		if (tokens == null || tokens.length == 0)
+			return 0;
+		String operators = "+-*/";
+		for(String i : tokens) {
+			if (!operators.contains(i)) {
+				stk.push(i);
+			} else {
+				int a = Integer.valueOf(stk.pop());
+				int b = Integer.valueOf(stk.pop());
+				switch(i){
+				case "+": stk.push(String.valueOf(a + b)); break;
+				case "-": stk.push(String.valueOf(b - a)); break;
+				case "*": stk.push(String.valueOf(a * b)); break;
+				case "/": stk.push(String.valueOf(b / a));
+				}
+			}
+		}
+		return Integer.valueOf(stk.pop());
     }
 }
